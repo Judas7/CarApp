c0b457b4e9e20287df5e19951971eee778017794
Linus Landin <linus.landin@edu.lycksele.se>
2021-04-22 11:46:05 +0200

    Started coding database class

---
+++ CarApp/database.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CarApp
+{
+    class database
+    {
+        public SQLiteConnection dbConn;
+        private string databaseFilename = "./car.db";
+
+        public Database()
+        {
+            if (!File.Exists(databaseFilename))
+            {
+                File.Create(databaseFilename);
+            }
+            dbConn = new SQLiteConnection("Data Source=" + databaseFilename); 
+
+        }
+
+        public void OpenConnection()
+        {
+            if (dbConn.State != System.Data.ConnectionState.Open)
+            {
+                dbConn.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (dbConn.State != System.Data.ConnectionState.Closed)
+            {
+                dbConn.Close();
+            }
+        }
+
+        public int AddCarRow(Car car)
+        {
+            string qInsert = "INSERT INTO car ('regNr', 'make', 'model', 'year', 'forSale') VALUES (@regNr, @make, @year, @forSale);";
+        }
+    }
+}
